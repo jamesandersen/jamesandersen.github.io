6bff066edad71a7dc60c7702e1c43fb4b5aa0a9f
James Andersen <james.j.andersen@gmail.com>
2013-08-06 00:15:24 -0600

    Working on adding media query styles to move sidebar to top at low res

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-(function (angular) {
+(function (angular, enquire) {
     
     angular.module('jander', []).
       config(function ($routeProvider) { // provider-injector
@@ -8,11 +8,25 @@
           // You can only inject Providers (not instances)
           // into the config blocks.
       }).
-      run(function () { // instance-injector
+      run(function ($rootScope, $log) { // instance-injector
           // This is an example of a run block.
           // You can have as many of these as you want.
           // You can only inject instances (not Providers)
           // into the run blocks
+          
+          enquire.register("screen and (max-width: 700px)", {
+              setup: function () {
+                  // Load in content via AJAX (just the once)
+                  $log.log('enquire loaded!');
+              },
+              match: function () {
+                  // Show sidebar
+                  $log.log('700px or less');
+              },
+              unmatch: function () {
+                  $log.log('more than 700px');
+              }
+          });
       });
 
     angular.module('jander').controller('SidebarCtrl', function($scope) {
@@ -25,4 +39,4 @@
         angular.bootstrap(document, ['jander', 'ngMobile']);
     });
     
-})(angular);
+})(angular, enquire);
