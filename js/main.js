6809f703b46c65591b7ab070435b0e5aa4a4297c
James Andersen <james.j.andersen@gmail.com>
2013-08-06 23:15:01 -0600

    More work on media queries for nav

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 (function (angular, enquire) {
-    
+    "use strict";
     angular.module('jander', []).
       config(function ($routeProvider) { // provider-injector
           // This is an example of config block.
@@ -14,26 +14,53 @@
           // You can only inject instances (not Providers)
           // into the run blocks
           
-          enquire.register("screen and (max-width: 700px)", {
-              setup: function () {
-                  // Load in content via AJAX (just the once)
-                  $log.log('enquire loaded!');
-              },
-              match: function () {
-                  // Show sidebar
-                  $log.log('700px or less');
-              },
-              unmatch: function () {
-                  $log.log('more than 700px');
-              }
-          });
+          
       });
 
-    angular.module('jander').controller('SidebarCtrl', function($scope) {
-        $scope.setSidebar = function($event, sidebar) {
-            
-        };
-    });
+    angular.module('jander').factory('navigation', ['$log', function($log) {
+        var navService = {};
+        var navStates = { DOCKED: 'DOCKED', MENU: 'MENU' };
+        var state = navStates.MENU, menuOpen = false;
+
+        enquire.register("screen and (max-width: 700px)", {
+            setup: function () {
+                // Load in content via AJAX (just the once)
+                $log.log('enquire loaded!');
+            },
+            match: function () {
+                state = navStates.MENU;
+                $log.log('700px or less');
+            },
+            unmatch: function () {
+                state = navStates.DOCKED;
+                $log.log('more than 700px');
+            }
+        });
+
+        //navService.docked = function () { return state === navStates.DOCKED; };
+        navService.toggleNavMenu = function () { if (state === navStates.MENU) menuOpen = !menuOpen; };
+        navService.navOpen = function () { return menuOpen; };
+        navService.docked = function () { return state === navStates.DOCKED; };
+
+        return navService;
+    }]);
+
+    angular.module('jander').controller('HeaderCtrl', ['$scope', 'navigation', function ($scope, navigation) {
+        $scope.toggleNav = navigation.toggleNavMenu;
+    }]);
+
+    angular.module('jander').controller('SidebarCtrl', ['$scope', 'navigation', function ($scope, navigation) {
+        $scope.navOpen = false, $scope.docked = true;
+        var unwatchNav = $scope.$watch(navigation.navOpen, function (newVal, oldVal) {
+            $scope.navOpen = newVal;
+            //$scope.$digest();
+        });
+        
+        var unwatchDocked = $scope.$watch(navigation.docked, function (newVal, oldVal) {
+            $scope.docked = newVal;
+            //$scope.$digest();
+        }, true);
+    }]);
 
     angular.element(document).ready(function () {
         angular.bootstrap(document, ['jander', 'ngMobile']);
